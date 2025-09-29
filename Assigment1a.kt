511ab0e8c70abb78f3c96651f88d4cf7cd17a0c3
madlib-doom~ <aaronplimo021@gmail.com>
2025-09-29 12:10:08 +0300

    first commit

---
+++ Assigment1a.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val number1:Int=100
+    val number2:Double=2.5
+
+    val total:Double=number1/number2
+
+    println("The average is: $total")
+
+    val numberr3:Int=50
+    val number4:Double=2.5
+
+    val product:Double=numberr3*number4
+
+    println("The product is: $product")
+
+}
