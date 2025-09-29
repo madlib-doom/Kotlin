8fa51575d9f5a15ca2ab4ac6d28658a5a6124d08
madlib-doom~ <aaronplimo021@gmail.com>
2025-09-29 11:37:04 +0300

    first commit

---
+++ Lesson1a.kt
@@ -0,0 +1,20 @@
+fun main(){
+    // Is a commnet
+    // A variable is a container that holds data value
+    // In kotlin we have two main types of initializers ie: var and val
+    // var-Valuehat keep on changing
+    // val-Don't change
+
+    val name="Aaron"  //string
+    val number1=5 //Integer data type(INT)
+    val number2=7.85  //Double`~Number with decimal
+    val letter="K" //char~Singleletter of alpahbet
+    val isRegistered=true //Boolean
+
+
+    println(name)
+    println(number1)
+    println(number2)
+    println(letter)
+    println(isRegistered)
+}
