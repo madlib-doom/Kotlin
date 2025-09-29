6deab4a535beade6ea86b445f62b58f70a540999
madlib-doom~ <aaronplimo021@gmail.com>
2025-09-29 11:57:02 +0300

    update  commit

---
+++ Lesson1b.kt
@@ -0,0 +1,15 @@
+fun main(){
+    // We shal specify the data types of he variables.
+    val number1:Int=5
+    val number2:Double=60.23
+    val letter:Char='D'
+    val name:String="Hesi"
+    val question:Boolean=false
+
+    println(number1)
+    println(number2)
+    println(letter)
+    println(name)
+    println(question)
+
+}
